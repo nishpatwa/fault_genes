3780cc8f569002913b3b0c0f08fbdf1e6053ebe6
Nish Patwa <nish.patwa@intel.com>
2017-04-15 04:40:49 +0000

    Added database connection to askopenstack data

--- fetch.py
+++ fetch.py
@@ -2,6 +2,8 @@ import requests
 import urllib,json
 import pandas as pd
 import string
+import sqlite3
+
 final_dict={}
 id_list=[]
 url_list = []
@@ -14,29 +16,58 @@ def getPageCount():
     return int(data['pages'])
 
 def fetchData(pages):
+ conn = sqlite3.connect('fault_genes.db')
+ conn.text_factory = str
+ print "Opened database successfully";
+
+ conn.execute('''CREATE TABLE IF NOT EXISTS askopenstack
+               (url TEXT,
+               summary TEXT,
+               title TEXT,
+               answer TEXT);''')
+
  for page in range(1, 10):
     parameters = {"scope": all, "page": page}
     response = requests.get("https://ask.openstack.org/en/api/v1/questions/", params = parameters)
     data = response.json()
     answer = 0
     for i in range(0,len(data['questions'])):
+       tmp_answer = ""
+       tmp_summary = ""
+       tmp_url = ""
+       tmp_title = ""
        for key,value in data['questions'][i].iteritems():
           url = data['questions'][i]['url'].encode('utf-8').strip()
           if 'url' in key:
              value = value.encode('utf-8').strip()
+             tmp_url = value
              url_list.append(value)
           elif 'summary' in key:
              value = value.encode('utf-8').strip()
+             tmp_summary = value
              summary_list.append(value)
           elif 'title' in key:
              value = value.encode('utf-8').strip()
+             tmp_title = value
              title_list.append(value)
           elif 'accepted_answer_id' in key:
               if value is None:
-                 answer = "-"
+                 answer = ""
               else: answer=getAnswer(value,url)
+              tmp_answer = answer
               answer_list.append(answer)
-              
+       row = {
+             'url':tmp_url,
+             'title':tmp_title,
+             'summary':tmp_summary,
+             'answer':tmp_answer
+       }
+       columns = ', '.join(row.keys())
+       placeholders = ', '.join('?' * len(row))
+       sql = 'INSERT INTO askopenstack ({}) VALUES ({})'.format(columns, placeholders)
+    #   import pdb;pdb.set_trace();
+       conn.execute(sql, row.values())
+       conn.commit()
     print "Completed Page#: "+str(page)
  final_dict['url'] = url_list
  final_dict['title'] = title_list
